c2900a44a7e3824fabc209d73be9cbcb21c3cd77
Stanislav Pavlov <pavlovstasmail@gmail.com>
2016-01-22 06:03:28 +0300

    gotoMarketYandexRu

--- src/test/java/test/ru/FirstTest.java
+++ src/test/java/test/ru/FirstTest.java
@@ -1,12 +1,7 @@
 package test.ru;
 
-/**
- * Created by stanislavpavlov on 22/01/16.
- */
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -21,24 +16,12 @@ public class FirstTest {
         driver = new FirefoxDriver();
     }
 
-    @Test (description="Launches the Yandex Market site")
+    @Test
     public void gotoMarketYandexRu() {
         driver.get("https://market.yandex.ru/");
         Assert.assertEquals(driver.getTitle(), "Яндекс.Маркет — покупки в тысячах проверенных магазинов");
     }
 
-    @Test (description="Go to Catalog")
-    public void gotoCatalog() {
-        driver.findElement(By.linkText("Каталог")).click();
-        driver.findElement(By.linkText("Популярные товары"));
-        driver.findElement(By.linkText("Вас также могут заинтересовать"));
-    }
-
-    @Test (description="Go to Electronics -> MobilePhones")
-    public void gotoMobilePhones() {
-        driver.findElement(By.linkText("Мобильные телефоны")).click();
-
-    }
 
     @AfterClass
     public void tearDown() {
