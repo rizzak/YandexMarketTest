5f6798f5a4faae42268fca07affc0d45a7120a13
Stanislav Pavlov <pavlovstasmail@gmail.com>
2016-01-22 06:46:11 +0300

    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);

--- src/test/java/test/ru/FirstTest.java
+++ src/test/java/test/ru/FirstTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.concurrent.TimeUnit;
+
 public class FirstTest {
 
     private WebDriver driver;
@@ -20,6 +22,9 @@ public class FirstTest {
     public void gotoMarketYandexRu() {
         driver.get("https://market.yandex.ru/");
         Assert.assertEquals(driver.getTitle(), "Яндекс.Маркет — покупки в тысячах проверенных магазинов");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.linkText("Каталог")).click();
     }
 
 
