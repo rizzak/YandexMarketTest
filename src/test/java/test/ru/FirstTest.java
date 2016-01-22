0a35e6258ede066b207769784491d9ffdd3f87a6
Stanislav Pavlov <pavlovstasmail@gmail.com>
2016-01-22 07:42:56 +0300

    Trying somethings

--- src/test/java/test/ru/FirstTest.java
+++ src/test/java/test/ru/FirstTest.java
@@ -1,7 +1,9 @@
 package test.ru;
 
+import org.apache.xpath.SourceTree;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -16,15 +18,25 @@ public class FirstTest {
     public void setUp() {
         // Create a new instance of the Firefox driver
         driver = new FirefoxDriver();
+        //driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @Test
     public void gotoMarketYandexRu() {
         driver.get("https://market.yandex.ru/");
+        // Check title
         Assert.assertEquals(driver.getTitle(), "Яндекс.Маркет — покупки в тысячах проверенных магазинов");
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.findElement(By.linkText("Каталог")).click();
+        // goto Catalog
+        driver.findElement(By.xpath(".//*[@id='index-headline-id-tab-1']/span[1]")).click();
+        for(WebElement x : (driver.findElements(By.tagName("h1")))) {
+            if (x.getText().equals("Популярные товары")){
+                System.out.println("good");
+            }
+            else{
+                System.out.printf("Bad");
+            }
+        }
     }
 
 
