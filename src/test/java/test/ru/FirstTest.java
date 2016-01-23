270f5b49829541b2ed0c6b4d1e4feee22ea6b1ba
Stanislav Pavlov <pavlovstasmail@gmail.com>
2016-01-23 05:52:09 +0300

    Need to fix "empty rating" only

--- src/test/java/test/ru/FirstTest.java
+++ src/test/java/test/ru/FirstTest.java
@@ -25,27 +25,51 @@ public class FirstTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         // Перейти по адресу https://market.yandex.ru/
         driver.get("https://market.yandex.ru/");
-        // Check title
+        // Проверка title
         Assert.assertEquals(driver.getTitle(), "Яндекс.Маркет — покупки в тысячах проверенных магазинов");
         // Нажать по ссылке "Каталог"
         driver.findElement(By.xpath(".//*[@id='index-headline-id-tab-1']/span[1]")).click();
+        // Проверка присутствия раздела "Популярные товары"
+        Assert.assertEquals(driver.findElement(By.className("main-bestsellers__title")).getText(), "Популярные товары");
         // Перейти по в раздел "Электроника" -> "Мобильные телефоны"
         driver.findElement(By.xpath("html/body/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[1]/a[7]")).click();
+        // проверка что в категории "Популярные" и "Новинки" представлены 3 девайса
+        List<WebElement> popList = driver.findElement(By.xpath("html/body/div[3]/table/tbody/tr/td[2]/div[1]/ul"))
+                                                                                        .findElements(By.tagName("li"));
+        Assert.assertEquals(popList.size(), 3);
+        List<WebElement> newList = driver.findElement(By.xpath("html/body/div[3]/table/tbody/tr/td[2]/div[2]/ul"))
+                                                                                        .findElements(By.tagName("li"));
+        Assert.assertEquals(newList.size(), 3);
         // Нажать по "расширенный поиск" блока выбора по параметрам
         driver.findElement(By.xpath("html/body/div[3]/table/tbody/tr/td[2]/table[1]/tbody/tr/td[1]/div/form/table/tbody/tr[6]/td/a")).click();
+        // Проверка title
+        Assert.assertEquals(driver.getTitle(), "Мобильные телефоны — выбор по параметрам на Яндекс.Маркете");
+        // Проверка что справа доступен блок параметров
+        Assert.assertEquals(true, driver.findElement(By.className("filter-panel-aside")).isDisplayed());
         // Ввести Цену, руб. "от" значение 5125
         driver.findElement(By.xpath(".//*[@id='gf-pricefrom-var']")).sendKeys("5125");
+        // Проверка что Значение успешно проставлено в поле "Цена от"
+        Assert.assertEquals(driver.findElement(By.xpath(".//*[@id='gf-pricefrom-var']")).getAttribute("value"), "5125");
         // Ввести Цену, руб. "до" значение 10123
         driver.findElement(By.xpath(".//*[@id='gf-priceto-var']")).sendKeys("10123");
+        // Проверка что Значение успешно проставлено в поле "Цена до"
+        Assert.assertEquals(driver.findElement(By.xpath(".//*[@id='gf-priceto-var']")).getAttribute("value"), "10123");
         // Кликнуть на чекбокс "В продаже"
         driver.findElement(By.xpath(".//*[@id='glf-in-stock-select']")).click();
+        // Проверить что Значение чекбокса "В продаже" установлено на "Выбрано"
+        Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='glf-in-stock-select']")).isSelected());
         // Раскрыть блок "Платформа"
         driver.findElement(By.xpath("html/body/div[1]/div[4]/div[2]/div[2]/div[1]/div/div[7]/div[1]")).click();
+        // Проверка что Успешно раскрыт блок параметра "Платформа"
+        Assert.assertEquals(true, driver.findElement(By.xpath("/html/body/div/div[4]/div[2]/div[2]/div[1]/div/div[7]/div[2]")).isDisplayed());
         // Кликнуть на селектбокс "Android"
         driver.findElement(By.xpath(".//*[@id='gf-2142557977--808187701']")).click();
+        // Проверка что Значение селектбокса "Android" установлено на "Выбрано"
+        Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='gf-2142557977--808187701']")).isSelected());
         // ожидание загрузки элементов
         (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.textToBePresentInElementLocated(By.className("title"), "Мобильные телефоны на Android в Нижнем Новгороде"));
+                .until(ExpectedConditions.textToBePresentInElementLocated(By.className("title")
+                                                                , "Мобильные телефоны на Android в Нижнем Новгороде"));
         // собираем все смартфоны в кучу
         List<WebElement> smartphones = driver.findElements(By.className("snippet-card"));
         // выбираем имеющих рейтинг от "3,5" до "4,5"
